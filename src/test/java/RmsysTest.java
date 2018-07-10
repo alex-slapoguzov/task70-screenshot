f44fe3df151e1affa3fb05483491cacbf96f47e7
Alexey Slapoguzov <alex.slapoguzov@gmail.com>
2018-07-10 10:21:29 +0300

    init repo

---
+++ src/test/java/RmsysTest.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class RmsysTest {
+
+    private WebDriver driver;
+    private final String URL = "https://192.168.100.26/";
+    private final String userName = "EugenBorisik";
+    private final String password = "qwerty12345";
+    private final String URL_HOME_PAGE = "https://192.168.100.26/Home/Index";
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(6000, TimeUnit.SECONDS);
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void rmsysLoginWithIdLocatorTest() {
+        WebElement userNameFildByIdLocator = driver.findElement(By.id("Username"));
+        WebElement passwordFildByIdLocator = driver.findElement(By.id("Password"));
+
+        userNameFildByIdLocator.sendKeys(userName);
+        passwordFildByIdLocator.sendKeys(password);
+        passwordFildByIdLocator.submit();
+        Assert.assertEquals(driver.getCurrentUrl(), URL_HOME_PAGE);
+    }
+}
